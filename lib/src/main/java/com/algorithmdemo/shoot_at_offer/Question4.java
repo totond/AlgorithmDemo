7afe33492eb5d25f7e6a908be8aff46bcfa05da1
yany <yanzhikai_yjk@qq.com>
2018-04-08 19:47:27 +0800

    add Question4

--- lib/src/main/java/com/algorithmdemo/shoot_at_offer/Question4.java
+++ lib/src/main/java/com/algorithmdemo/shoot_at_offer/Question4.java
@@ -4,11 +4,17 @@ package com.algorithmdemo.shoot_at_offer;
  * author : yany
  * e-mail : yanzhikai_yjk@qq.com
  * time   : 2018/03/04
- * desc   :
+ * desc   : 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ *          请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
  */
 
 public class Question4 {
 
+    //先从左上角的数开始判断，
+    //如果目标数小于当前数，则说明这一列没有目标数（都比它大），当前数往左移
+    //如果目标数大于当前数，则说明这一行没有目标数（都比它小），当前数往下移
+    //如果等于，则说明找到，
+    //如果已经移出边界，则说明没有。
     public boolean Find(int target, int [][] array) {
         if (array == null){
             return false;
